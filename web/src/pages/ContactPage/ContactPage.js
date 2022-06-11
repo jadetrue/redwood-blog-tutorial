41b2482913451b7446b01c3d9ab20eb5411d6e7d
Jade True <jadetruexo@gmail.com>
2022-06-11 21:37:35 +0100

    Chapter 3 Saving Data from form

--- web/src/pages/ContactPage/ContactPage.js
+++ web/src/pages/ContactPage/ContactPage.js
@@ -1,24 +1,54 @@
-import { MetaTags } from '@redwoodjs/web'
+import { MetaTags, useMutation } from '@redwoodjs/web'
+import { toast, Toaster } from '@redwoodjs/web/toast'
 import {
-  Form,
-  Submit,
-  TextField,
-  TextAreaField,
   FieldError,
+  Form,
+  FormError,
+  Label,
+  Submit,
+  TextAreaField,
+  TextField,
+  useForm,
 } from '@redwoodjs/forms'
 
-const ContactPage = () => {
-  const onSubmit = (data) => {
-    console.log(data)
+const CREATE_CONTACT = gql`
+  mutation CreateContactMutation($input: CreateContactInput!) {
+    createContact(input: $input) {
+      id
+    }
   }
+`
+
+const ContactPage = () => {
+  const formMethods = useForm()
+
+  const [create, { loading, error }] = useMutation(CREATE_CONTACT, {
+    onCompleted: () => {
+      toast.success('Thank you for your submission!')
+      formMethods.reset()
+    },
+  })
+
+  const onSubmit = (data) => {
+    create({ variables: { input: data } })
+  }
+
   return (
     <>
       <MetaTags title="Contact" description="Contact page" />
 
-      <Form onSubmit={onSubmit} config={{ mode: 'onBlur' }}>
-        <label htmlFor="name" errorClassName="error">
+      <Toaster />
+      <Form
+        onSubmit={onSubmit}
+        config={{ mode: 'onBlur' }}
+        error={error}
+        formMethods={formMethods}
+      >
+        <FormError error={error} wrapperClassName="form-error" />
+
+        <Label name="name" errorClassName="error">
           Name
-        </label>
+        </Label>
         <TextField
           name="name"
           validation={{ required: true }}
@@ -26,9 +56,9 @@ const ContactPage = () => {
         />
         <FieldError name="name" className="error" />
 
-        <label htmlFor="email" errorClassName="error">
+        <Label name="email" errorClassName="error">
           Email
-        </label>
+        </Label>
         <TextField
           name="email"
           validation={{
@@ -42,9 +72,9 @@ const ContactPage = () => {
         />
         <FieldError name="email" className="error" />
 
-        <label htmlFor="message" errorClassName="error">
+        <Label name="message" errorClassName="error">
           Message
-        </label>
+        </Label>
         <TextAreaField
           name="message"
           validation={{ required: true }}
@@ -52,7 +82,7 @@ const ContactPage = () => {
         />
         <FieldError name="message" className="error" />
 
-        <Submit>Save</Submit>
+        <Submit disabled={loading}>Save</Submit>
       </Form>
     </>
   )
