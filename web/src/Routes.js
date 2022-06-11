9d6960708080d6e1960d4662961c43c0f6576c3f
Jade True <jadetruexo@gmail.com>
2022-06-11 21:17:48 +0100

    Chapter 3 adding forms

--- web/src/Routes.js
+++ web/src/Routes.js
@@ -24,6 +24,7 @@ const Routes = () => {
         <Route path="/about" page={AboutPage} name="about" />
         <Route path="/" page={HomePage} name="home" />
         <Route path="/article/{id:Int}" page={ArticlePage} name="article" />
+        <Route path="/contact" page={ContactPage} name="contact" />
       </Set>
       <Route notfound page={NotFoundPage} />
     </Router>
---
+++ web/src/pages/ContactPage/ContactPage.js
@@ -0,0 +1,61 @@
+import { MetaTags } from '@redwoodjs/web'
+import {
+  Form,
+  Submit,
+  TextField,
+  TextAreaField,
+  FieldError,
+} from '@redwoodjs/forms'
+
+const ContactPage = () => {
+  const onSubmit = (data) => {
+    console.log(data)
+  }
+  return (
+    <>
+      <MetaTags title="Contact" description="Contact page" />
+
+      <Form onSubmit={onSubmit} config={{ mode: 'onBlur' }}>
+        <label htmlFor="name" errorClassName="error">
+          Name
+        </label>
+        <TextField
+          name="name"
+          validation={{ required: true }}
+          errorClassName="error"
+        />
+        <FieldError name="name" className="error" />
+
+        <label htmlFor="email" errorClassName="error">
+          Email
+        </label>
+        <TextField
+          name="email"
+          validation={{
+            required: true,
+            pattern: {
+              value: /^[^@]+@[^.]+\..+$/,
+              message: 'Please enter a valid email address',
+            },
+          }}
+          errorClassName="error"
+        />
+        <FieldError name="email" className="error" />
+
+        <label htmlFor="message" errorClassName="error">
+          Message
+        </label>
+        <TextAreaField
+          name="message"
+          validation={{ required: true }}
+          errorClassName="error"
+        />
+        <FieldError name="message" className="error" />
+
+        <Submit>Save</Submit>
+      </Form>
+    </>
+  )
+}
+
+export default ContactPage
